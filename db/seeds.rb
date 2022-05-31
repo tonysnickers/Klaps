4d9eada92bb871f2bf67b8de2a3707104c71dc5e
Jeanne dL <jeanne.deleusse@gmail.com>
2022-05-31 11:26:25 +0200

    seed sans routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "https://api.themoviedb.org/3/movie/555?api_key=5a07d55b0507c919cb598bae7c6fd7b4"
+10.times do |i|
+  movies = JSON.parse(URI.open("https://api.themoviedb.org/3/movie/#{i}?api_key=5a07d55b0507c919cb598bae7c6fd7b4").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      synopsis: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
+puts "Movies created"
