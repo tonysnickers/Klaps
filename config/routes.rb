b6cbcb473867265d4e4775577b74402abc143838
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-31 15:10:55 +0200

    dashboard to be finished, detailled path and css

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'dashboard', to: 'dashboards#dashboard'
-  get 'dashboard', to: 'dashboards#index', as: :dashboard_index
+  get 'dashboard/index', to: 'dashboards#index', as: :dashboard_index
 
   resources :group, only: [:new] do
     resources :ordered_choices, only: %i[new edit]
