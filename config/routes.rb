3113f980d415200d06081a8f935807ae18528178
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-31 10:31:24 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  ressources :
+  get 'dashboard', to: 'dashboards#dashboard'
+  get 'dashboard', to: 'dashboards#index'
+
+  resources :group, only: [:new] do
+    resources :ordered_choices, only: %i[new edit]
+    resources :quizz_choices, only: %i[new edit]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
