aced2ebb384fb1e1714052f37079dcc0564929b9
Manoa Rasolomalala <manoatony@gmail.com>
2022-06-01 10:46:32 +0200

    routes add for solo

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'dashboards#dashboard'
   get 'dashboard/index', to: 'dashboards#index'
 
-  resources :groups, only: [:index, :show, :new, :create, :update] do
+  resources :groups, only: %i[index show new create update] do
     resources :ordered_choices, only: %i[new edit]
     resources :quizz_choices, only: %i[new edit]
   end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :quizz_choices, only: %i[new edit]
 end
