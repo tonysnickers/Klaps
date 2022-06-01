d7ac5b2f4b4c54c5178014b453d67901aef24ef4
antoine mendy <antoine.mendy@hotmail.fr>
2022-06-01 14:41:17 +0200

    test

--- app/controllers/quizz_choices_controller.rb
+++ app/controllers/quizz_choices_controller.rb
@@ -7,13 +7,16 @@ class QuizzChoicesController < ApplicationController
   def create
     # create le quizz choice
     # insert les différents genres via les params dans la colonne genre
-    # quizz_choice.group = Group.find(params[:group_id])
-    # quizz_choice.user = current_user
-    # if quizz_choice.save
-    # else
-    # end
+    @quizz_choice.group = Group.find(params[:group_id])
+    @quizz_choice.user = current_user
+    if @quizz_choice.save
+      redirect_to
+    else
+
+    end
   end
 
   def edit
   end
+
 end
