633c2aced4ef8fbb2b4bc9b91ab5f8f578e8ae34
Manoa Rasolomalala <manoatony@gmail.com>
2022-06-07 23:40:40 +0200

    carousel need modif

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,5 @@
 class DashboardsController < ApplicationController
   def dashboard
-    # @movies = policy_scope(Movie)
-    # @movies_max = []
-    # @movies = @movies_max.tally.max_by(5) { |key, value| value }.map { |a| a[0] }
   end
 
   def index
