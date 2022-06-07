6dace28f1493f6610e071e24a2252c941e30b9d1
Manoa Rasolomalala <manoatony@gmail.com>
2022-06-07 23:09:16 +0200

    carousel

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,8 @@
 class DashboardsController < ApplicationController
   def dashboard
+    # @movies = policy_scope(Movie)
+    # @movies_max = []
+    # @movies = @movies_max.tally.max_by(5) { |key, value| value }.map { |a| a[0] }
   end
 
   def index
