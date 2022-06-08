0817c52c70d74284208d25dcdb7244ea438c79a2
tonysnickers <antoine.mendy@hotmail.fr>
2022-06-08 17:11:00 +0200

    Super ca fuse

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ApplicationRecord
   has_many :ordered_choices, dependent: :destroy
   has_many :wishes, dependent: :destroy
-  has_many :movie_seens, dependent: :destroy
 
   serialize :keyword, Array
   serialize :actor, Array
